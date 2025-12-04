70779ed079801b74619a732dd60b422a911985af
smilee1111 <muskankc2005@gmail.com>
2025-12-05 01:36:54 +0545

    minor fixes for login and signup

--- lib/screens/LoginPageScreen.dart
+++ lib/screens/LoginPageScreen.dart
@@ -15,154 +15,151 @@ class _LoginpagescreenState extends State<Loginpagescreen> {
   final _formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+return Scaffold(
+  backgroundColor: AppColors.background,
+  appBar: AppBar(
     backgroundColor: AppColors.background,
-    appBar: AppBar(
-      backgroundColor: AppColors.background,
-    ),
-    body: 
-    Form(
-      key: _formKey,
-      child: Padding(
-        padding: const EdgeInsets.all(20),
+  ),
+  body: SafeArea(
+    child: SingleChildScrollView(
+      padding: const EdgeInsets.all(20),
+      child: Form(
+        key: _formKey,
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
             Row(
               children: [
-                Text('CLICK\n\nTYPE\n\nFIND\n\nyour shop.',
-                style: AppTextStyle.h1, 
+                Text(
+                  'CLICK\n\nTYPE\n\nFIND\n\nyour shop.',
+                  style: AppTextStyle.h1,
                 ),
-                SizedBox(width: 120),
-                Image.asset('assets/images/shopping bag img.png',
-                width: 100,
-                height: 200,)
+                const SizedBox(width: 120),
+                Image.asset(
+                  'assets/images/shopping bag img.png',
+                  width: 100,
+                  height: 200,
+                ),
               ],
             ),
-            SizedBox(height: 55),
-             TextFormField(
+
+            const SizedBox(height: 55),
+
+            // USERNAME
+            TextFormField(
               controller: usernameController,
               keyboardType: TextInputType.text,
               style: AppTextStyle.inputBox,
               decoration: InputDecoration(
                 labelText: "USERNAME",
                 hintText: "e.g: John Doe",
-                border:  OutlineInputBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
                 ),
-                enabledBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(10.0)),
-                  borderSide: BorderSide(color: AppColors.accent)
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(10.0)),
-                  borderSide: BorderSide(color: AppColors.accent),
-                )
               ),
-              validator: (value){
-                if(value==null || value.isEmpty){
+              validator: (value) {
+                if (value == null || value.isEmpty) {
                   return "Please enter your full name.";
                 }
                 return null;
-                },
+              },
             ),
-            SizedBox(height: 20),
-            Expanded(
-            child:
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [ 
-              TextFormField(
+
+            const SizedBox(height: 20),
+
+            // PASSWORD
+            TextFormField(
               controller: passwordController,
-              keyboardType: TextInputType.text,
+              obscureText: true,
               style: AppTextStyle.inputBox,
               decoration: InputDecoration(
                 labelText: "PASSWORD",
-                suffixIcon: IconButton(onPressed: () {}, icon: Icon(Icons.visibility)),
-                hintText: "e.g: John Doe",
-                border:  OutlineInputBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                suffixIcon: Icon(Icons.visibility),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
                 ),
-                enabledBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(10.0)),
-                  borderSide: BorderSide(color: AppColors.accent)
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(10.0)),
-                  borderSide: BorderSide(color: AppColors.accent),
-                )
               ),
-              validator: (value){
-                if(value==null || value.isEmpty){
-                  return "Please enter your full name.";
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return "Please enter your password.";
                 }
                 return null;
-                },
+              },
             ),
-            TextButton(onPressed: () {}, child: Text("Forgot Password?",
-            style: AppTextStyle.minimalTexts.copyWith(
-            decoration: TextDecoration.underline,))),
-             SizedBox(
+
+            TextButton(
+              onPressed: () {},
+              child: Text(
+                "Forgot Password?",
+                style: AppTextStyle.minimalTexts.copyWith(
+                  decoration: TextDecoration.underline,
+                ),
+              ),
+            ),
+
+            SizedBox(
               width: 150,
               child: ElevatedButton(
                 onPressed: () {
-                  if(_formKey.currentState!.validate()){
+                  if (_formKey.currentState!.validate()) {
                     Navigator.pushNamed(context, '/DashboardScreen');
                   }
-                  
-                }, 
+                },
                 style: ElevatedButton.styleFrom(
-                backgroundColor: const Color.fromRGBO(82, 70, 50, 1),
-                minimumSize: Size(double.infinity, 50),
-                shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10)
-                )
+                  backgroundColor: const Color.fromRGBO(82, 70, 50, 1),
+                  minimumSize: const Size(double.infinity, 50),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
                 ),
-                child: Text("LOGIN",
-                style: AppTextStyle.buttonText,),
+                child: Text(
+                  "LOGIN",
+                  style: AppTextStyle.buttonText,
+                ),
               ),
             ),
-            TextButton(onPressed: () {
-              Navigator.pushNamed(context, '/RegisterScreen');
-            }, child: Text("Don't have an account?",
-            style: AppTextStyle.minimalTexts.copyWith(
-            decoration: TextDecoration.underline,))),
-            SizedBox(height: 15),
-              Container(
-              width:500,
-              height: 85,
-              alignment: Alignment.centerRight,
-              decoration: BoxDecoration(
-                shape: BoxShape.rectangle,
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(10)
+
+            TextButton(
+              onPressed: () {
+                Navigator.pushNamed(context, '/RegisterScreen');
+              },
+              child: Text(
+                "Don't have an account?",
+                style: AppTextStyle.minimalTexts.copyWith(
+                  decoration: TextDecoration.underline,
                 ),
-                child:
-                Padding(
-                  padding: const EdgeInsets.all(10.0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text('Continue with Google', 
-                      style: AppTextStyle.minimalTexts,),
-                      SizedBox(width: 20),
-                      Image.network('https://upload.wikimedia.org/wikipedia/commons/thumb/3/3c/Google_Favicon_2025.svg/960px-Google_Favicon_2025.svg.png',
-                      width: 40,
-                      height: 40)
-                    ],
+              ),
+            ),
+
+            const SizedBox(height: 15),
+
+            Container(
+              width: double.infinity,
+              height: 85,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text('Continue with Google', style: AppTextStyle.minimalTexts),
+                  const SizedBox(width: 20),
+                  Image.network(
+                    'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3c/Google_Favicon_2025.svg/960px-Google_Favicon_2025.svg.png',
+                    width: 40,
+                    height: 40,
                   ),
-                )
-              )
-              ]
-              
-            )
-                      )
-             
+                ],
+              ),
+            ),
+
+            const SizedBox(height: 40),
           ],
         ),
       ),
-    )
-    );
+    ),
+  ),
+);
   }
 }
